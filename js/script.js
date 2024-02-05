fcbcaaa95432ce6773ac802d40c000e5d0181066
bk7191 <bk7191014@gmail.com>
2024-02-05 22:59:27 +0100

    v 0.1 css manque cadre pour video

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-const api_url = "https://localhost:8000/api/v1/titles/";
+const api_url = "http://localhost:8000/api/v1/titles/";
+const
+async function fetchHighestRatedMovie() {
+    return fetch(api_url + "?sort_by=-imdb_score")
+    .then(response=>response.json())
+    .then(data => {
+        const movie = data.results[0];
+        const title = movie.title;
+        const imageUrl = movie.image_url;
+
+        return movie;})
+
+
+
+}
+
+async function fetchBestMovie() {
+  console.log('calling');
+  const response = await fetch(api_url + "?sort_by=-imdb_score");
+  const data = await response.json();
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // Expected output: "resolved"
+}
+fetchHighestRatedMovie()
+fetchBestMovie();
 
 // move carousel to right
-for (
-)
+
 
 // move carousel to left
 
