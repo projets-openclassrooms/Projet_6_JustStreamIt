4bf2c59c69bf27968d45ce632487728f6f9eb899
bk7191 <bk7191014@gmail.com>
2024-02-05 16:24:11 +0100

    v 0.1 css manque cadre pour video

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const api_url = "https://localhost:8000/api/v1/titles/";
+
+// move carousel to right
+for (
+)
+
+// move carousel to left
+
+// display carousel
+
+// close carousel
+
+// Top rated cover
+
+// button
+
+// fetch Top Rated Movies (number <=7)
+
+// fetch Best Movies cat 1 (number <=7)
+// fetch Best Movies cat 2 (number <=7)
+// fetch Best Movies cat 3 (number <=7)
