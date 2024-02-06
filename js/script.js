588bc77653288c479cab8e79bef4455b6fb60685
bk7191 <bk7191014@gmail.com>
2024-02-06 18:44:21 +0100

    Revert "v 0.1 css manque cadre pour video"
    This reverts commit fcbcaaa95432ce6773ac802d40c000e5d0181066.

--- js/script.js
+++ js/script.js
@@ -1,32 +1,8 @@
-const api_url = "http://localhost:8000/api/v1/titles/";
-const
-async function fetchHighestRatedMovie() {
-    return fetch(api_url + "?sort_by=-imdb_score")
-    .then(response=>response.json())
-    .then(data => {
-        const movie = data.results[0];
-        const title = movie.title;
-        const imageUrl = movie.image_url;
-
-        return movie;})
-
-
-
-}
-
-async function fetchBestMovie() {
-  console.log('calling');
-  const response = await fetch(api_url + "?sort_by=-imdb_score");
-  const data = await response.json();
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-  // Expected output: "resolved"
-}
-fetchHighestRatedMovie()
-fetchBestMovie();
+const api_url = "https://localhost:8000/api/v1/titles/";
 
 // move carousel to right
-
+for (
+)
 
 // move carousel to left
 
