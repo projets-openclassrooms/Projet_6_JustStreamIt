3b8906a3b3a4b32ec948b96603d9d9d1f26c44a4
bk7191 <bk7191014@gmail.com>
2024-02-07 21:48:10 +0100

    v 0.2

--- js/script.js
+++ js/script.js
@@ -1,56 +1,56 @@
-const api_url = "https://localhost:8000/api/v1/titles/";
-    const response = await fetch(api_url + "?sort_by=-imdb_score");
-    const data = await response.json();
-async function fetchBestMovie(){
-try{
-    let bestImage = document.getElementById("best-cover-img");
-    bestImage.src = data.results[0].image_url;
+const API_URL = "http://localhost:8000/api/v1/titles/";
+async function loadNames(){const response = await fetch(API_URL); const names = await response.json(); console.log(names);}
 
-
-
-
-}
-}
-// move carousel to right
-
-
-// move carousel to left
-
-// display carousel
+loadNames();
+function fetchBestMovie(){
+    const cover = document.getElementById('movie-cover');
+    cover.style.display = 'block';
+    let bestTitle = document.getElementByClassName("movie-cover")[0].getElementById("title")[0];
+    let bestDescription = document.document.getElementByClassName("movie-cover");
+    fetch(API_URL + "?sort_by=-imdb_score")
+    .then((response)=> response.json())
+    .then((data) =>{
+    bestTitle.innerHTML = data["title"];
+    bestDescription.innerHTML = data["description"]; })
+    .catch(function(error){
+    cover.style.display = 'none';
+    console.log("Erreur dans la restitution", error);
+    })
+    ;
+    }
 
 // close carousel
-
+$('div#modal').on('click',function(event){
+    event.preventDefault();
+    $('div#modal').fadeOut('slow');
+    $('h3,p,strong').removeClass('blured_text');
+});
 // Top rated cover
 
 // button
 // modal
 // Get the modal
-var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var button = document.getElementById("myBtn");
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+
 
 // When the user clicks on the button, open the modal
-button.onclick = function() {
-  modal.style.display = "block";
-}
+
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
 // fetch Top Rated Movies (number <=7)
 
 // fetch Best Movies cat 1 (number <=7)
 // fetch Best Movies cat 2 (number <=7)
 // fetch Best Movies cat 3 (number <=7)
+function main(){
+window.addEventListener('load',() =>{
+fetchBestMovie()});
+}
